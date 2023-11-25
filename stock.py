5b515f6e347d48d26a0338cde4f05bb7f99556d0
Cj Harris <cj11ty7@gmail.com>
2023-11-25 17:31:43 -0600

    uodated the plotting function

--- stock.py
+++ stock.py
@@ -18,7 +18,17 @@ def process_stock_data(data):
     df.index = pd.to_datetime(df.index)
     return df.sort_index()
 
-def plot_and_save_stock_data(df, symbol, chart_type, start_date, end_date, save_path):
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from pandas import to_datetime
+
+def plot_and_save_stock_data(df, symbol, chart_type, start_date, end_date, save_path, column='Close'):
+    # Check if DataFrame is empty or if the specified column doesn't exist
+    if df.empty:
+        raise ValueError("The provided DataFrame is empty.")
+    if column not in df.columns:
+        raise ValueError(f"The column '{column}' does not exist in the DataFrame.")
+
     # Convert start and end dates to datetime, handling empty or invalid inputs
     if start_date:
         start_date = to_datetime(start_date)
@@ -30,24 +40,34 @@ def plot_and_save_stock_data(df, symbol, chart_type, start_date, end_date, save_
     else:
         end_date = df.index.max()  # use latest date in the DataFrame
 
+    # Ensure that start_date is before end_date
+    if start_date > end_date:
+        raise ValueError("Start date must be before end date.")
+
     # Filter the DataFrame by date range
     df = df.loc[start_date:end_date]
 
+    # Check if the filtered DataFrame is empty
+    if df.empty:
+        raise ValueError("No data available for the given date range.")
+
     # Create the plot
-    ax = plt.subplots()
-    
+    fig, ax = plt.subplots()
+
     # Plot the data according to chart type
     if chart_type == "line":
-        ax.plot(df.index, df["Close"], label="Close", color='green')
+        ax.plot(df.index, df[column], label=column, color='green')
     elif chart_type == "bar":
-        ax.bar(df.index, df["Close"], label="Close", color='blue')
-        
-    plt.title(f"{symbol} Prices from {start_date} to {end_date}")
+        ax.bar(df.index, df[column], label=column, color='blue')
+    else:
+        raise ValueError("Unsupported chart type. Please use 'line' or 'bar'.")
+
+    plt.title(f"{symbol} {column} Prices from {start_date.date()} to {end_date.date()}")
     plt.xlabel("Date")
     plt.ylabel("Price")
 
     # Set y-axis to start at 0 and dynamically set the upper limit based on the data
-    ax.set_ylim(bottom=0, top=max(df["Close"])*1.1)  # 10% padding above the max value
+    ax.set_ylim(bottom=0, top=max(df[column])*1.1)  # 10% padding above the max value
 
     # Set the maximum number of y-axis ticks to 5
     ax.yaxis.set_major_locator(MaxNLocator(nbins=5))
@@ -55,8 +75,10 @@ def plot_and_save_stock_data(df, symbol, chart_type, start_date, end_date, save_
     # Rotate x-axis labels
     plt.setp(ax.get_xticklabels(), rotation=45)
 
-    plt.legend()  # Add a legend to distinguish the close line
+    plt.legend()  # Add a legend to distinguish the plotted column
 
     # Save the figure
     plt.savefig(save_path, dpi=300)
     plt.close()
+
+    return f"Plot saved successfully at {save_path}"
